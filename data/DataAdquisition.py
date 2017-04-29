4cdf420f80c72004499500b5b598df6b3f554c8c
jorgenav <jornaga1@gmail.com>
2017-04-29 15:28:50 +0200

    Updated code and uploaded json with preprocessed twitter data.

--- data/DataAdquisition.py
+++ data/DataAdquisition.py
@@ -1,69 +1,103 @@
+# -*- coding: utf-8 -*-
+
 import twitter
-# from twitter import *
+from twitter import TwitterError
+from collections import OrderedDict
 import json
+import time
 
-#Twitter app information
-# KEY - 1
-# consumer_secret='P6sc6XsM6h2ne9XZtT1RKHRCODb4snHKEp1MmA9jloMhEaSnbA'
-# consumer_key='5zlB3yBmTW04agzm3KoBUknft'
-# access_token='796306387903070208-HTGwSGUE4hqnjQdCG4M0zfeLPrwKPOK'
-# access_token_secret='6YtVR1OLvbwteaEL5jS6QpIFmBqJ3eKfdorL0yNgwIhEC'
+LIMIT_REACHED_ERROR = 88
 
-# KEY - 2
-# consumer_secret='FuH4nRi6DLEAeWTtkFk0rltSPqCZLffHugSIS2zJ4gg1sDFdB7'
-# consumer_key='b7qyY4pXGA7lNmduwYD3CZvkC'
-# access_token='796306387903070208-E2BVN16Ye9au8EhjPp0DTA3XqdKpDKF'
-# access_token_secret='g4dXXuB2FN6XhJgTI1ali4xHp72fqP9hGhoG4ksSL3iyW'
 
-# # KEY - 3
-consumer_secret='gGMatXDbkzmd98Zlcpp0iIAaQ4oVoep6FYETc54vPKCjbZ3vm6'
-consumer_key='0NFMwVw2kk3ozt2ee8JbC1YJR'
-access_token='796306387903070208-sl3weFGdmpHrcnWpdcIHYjC09ro1TYq'
-access_token_secret='R7Y0RHniM39NjNMsU5wth5JddCfnd2d1hV115VLgLIOhL'
+class KeyStore:
 
-# # KEY - 4
-# consumer_secret=''
-# consumer_key=''
-# access_token=''
-# access_token_secret=''
+    def __init__(self, keyfile):
+        self.keys = [line.rstrip('\n') for line in open(keyfile)]
+        self.idx = 0
+        key = self.keys[self.idx].split()
+        self.api = twitter.Api(consumer_key=key[0], consumer_secret=key[1],
+                               access_token_key=key[2], access_token_secret=key[3])
 
-# # KEY - 5
-# consumer_secret=''
-# consumer_key=''
-# access_token=''
-# access_token_secret=''
+    def size(self):
+        return len(self.keys)
+
+    def change_credentials(self):
+        self.idx = (self.idx + 1) % len(self.keys)
+        key = self.keys[self.idx].split()
+        self.api = twitter.Api(consumer_key=key[0], consumer_secret=key[1],
+                               access_token_key=key[2], access_token_secret=key[3])
 
 
 if __name__ == '__main__':
-    #Logging
-    # auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
-    # auth = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret,
-    #                    access_token_key=access_token, access_token_secret=access_token_secret)
-    # api = twitter.Twitter(auth=auth)
+    key_store = KeyStore('../../twitter_keys.txt')
+    tries = 0
+    requests = 0
 
-    api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret,
-                       access_token_key=access_token, access_token_secret=access_token_secret)
+    # following = key_store.api.GetFriendIDs(screen_name='coralpintado')
+    following = key_store.api.GetFriendIDs(screen_name='manubarba')
 
-    # api = twitter.Api(consumer_key=[consumer key],
-    #                   consumer_secret=[consumer secret],
-    #                   access_token_key=[access token],
-    #                   access_token_secret=[access token secret])
+    # followers = key_store.api.GetFollowers(screen_name='coralpintado')
+    followers = key_store.api.GetFollowers(screen_name='manubarba')
 
+    i = 1
 
-    list = []
-    cursor=-1
-    while cursor != 0:
-        users = api.GetFollowers(screen_name='coralpintado', cursor = cursor)
-        # users = api.friends.list(screen_name = 'coralpintado', cursor = cursor)
-        # print(users)
-        list.append(users)
-        print(users['next_cursor'])
-        cursor = int(users['next_cursor'])
+    dict_json = OrderedDict()
+    lst_nodes = []
 
-    # print json.dumps(,indent=1)
+    lst_dict_json = []
 
-    with open("data_twitter.json", "w") as file:
-        for line in list:
-            file.write(json.dumps(line))
-        print("Finished")
-        # file.write(list)
+    # with open('data_twitter_C.json', 'w') as f:
+    with open('data_twitter_M_test.json', 'w') as f:
+        for u in followers:
+            for u2 in following:
+                if (u.id == u2) and (not u.protected):
+                    try:
+                        # Número de usuario -> Crear
+                        # dict_json['id'] = i
+                        dict_json['id'] = u.id
+                        i += 1
+                        # Nombre
+                        dict_json['name'] = u.name
+                        # Número de seguidores
+                        dict_json['n_followers'] = u.followers_count
+                        # Número de personas a las que sigue el usuario
+                        dict_json['n_following'] = u.friends_count
+                        # Enlace http a la imagen del usuario
+                        dict_json['img'] = u.profile_image_url
+                        # Alias (nombre tras @)
+                        dict_json['alias'] = u.screen_name
+                        # Número de tweets del usuario
+                        dict_json['n_tweets'] = u.statuses_count
+                        # Geolocalización de tweets
+                        dict_json['geo'] = u.geo_enabled
+                        # User location -> Useful?
+                        dict_json['location'] = u.location
+                        # Cuenta verificada
+                        dict_json['verified'] = u.verified
+
+                        # Almacenamiento de seguidos por el usuario
+                        dict_json['following'] = key_store.api.GetFriendIDs(screen_name=u.screen_name)
+
+                        lst_dict_json.append(dict_json.copy())
+
+                        # Se añade el usuario a la lista de nodos
+                        lst_nodes.append(u.id)
+
+                    except TwitterError as e:
+                        if str(e.message) in ("[{'message': 'Rate limit exceeded', 'code': 88}]",
+                                              "[{'code': 88, 'message': 'Rate limit exceeded'}]"):
+                            tries += 1
+                            if tries == len(key_store.keys):
+                                print('All credentials limit reached, sleeping ...')
+                                tries = 0
+                                time.sleep(60)
+                            else:
+                                requests = 0
+                                print('Rate limit reached with credential ', key_store.idx, ' trying next ...')
+                                key_store.change_credentials()
+                    except:
+                        print('Generic error, retrying in one minute')
+                        time.sleep(60)
+
+        for user in lst_dict_json:
+            f.write(json.dumps(user, ensure_ascii=False) + '\n')
--- data/DataAdquisition2.py
+++ data/DataAdquisition2.py
@@ -1,72 +1,184 @@
+# -*- coding: utf-8 -*-
+
 import twitter
-import pandas as pd
+from twitter import TwitterError
+from collections import OrderedDict
+import json
+import time
 
-#Twitter app information
-# KEY - 1
-consumer_secret='P6sc6XsM6h2ne9XZtT1RKHRCODb4snHKEp1MmA9jloMhEaSnbA'
-consumer_key='5zlB3yBmTW04agzm3KoBUknft'
-access_token='796306387903070208-HTGwSGUE4hqnjQdCG4M0zfeLPrwKPOK'
-access_token_secret='6YtVR1OLvbwteaEL5jS6QpIFmBqJ3eKfdorL0yNgwIhEC'
 
-# KEY - 2
-# consumer_secret='FuH4nRi6DLEAeWTtkFk0rltSPqCZLffHugSIS2zJ4gg1sDFdB7'
-# consumer_key='b7qyY4pXGA7lNmduwYD3CZvkC'
-# access_token='796306387903070208-E2BVN16Ye9au8EhjPp0DTA3XqdKpDKF'
-# access_token_secret='g4dXXuB2FN6XhJgTI1ali4xHp72fqP9hGhoG4ksSL3iyW'
+LIMIT_REACHED_ERROR = 88
 
-# # KEY - 3
-# consumer_secret='gGMatXDbkzmd98Zlcpp0iIAaQ4oVoep6FYETc54vPKCjbZ3vm6'
-# consumer_key='0NFMwVw2kk3ozt2ee8JbC1YJR'
-# access_token='796306387903070208-sl3weFGdmpHrcnWpdcIHYjC09ro1TYq'
-# access_token_secret='R7Y0RHniM39NjNMsU5wth5JddCfnd2d1hV115VLgLIOhL'
 
-# # KEY - 4
-# consumer_secret=''
-# consumer_key=''
-# access_token=''
-# access_token_secret=''
+class KeyStore:
 
-# # KEY - 5
-# consumer_secret=''
-# consumer_key=''
-# access_token=''
-# access_token_secret=''
+    def __init__(self, keyfile):
+        self.keys = [line.rstrip('\n') for line in open(keyfile)]
+        self.idx = 0
+        key = self.keys[self.idx].split()
+        self.api = twitter.Api(consumer_key=key[0], consumer_secret=key[1],
+                               access_token_key=key[2], access_token_secret=key[3])
+
+    def size(self):
+        return len(self.keys)
+
+    def change_credentials(self):
+        self.idx = (self.idx + 1) % len(self.keys)
+        key = self.keys[self.idx].split()
+        self.api = twitter.Api(consumer_key=key[0], consumer_secret=key[1],
+                               access_token_key=key[2], access_token_secret=key[3])
+
+
+# if __name__ == '__main__':
+#     key_store = KeyStore('../../twitter_keys.txt')
+#     tries = 0
+#     requests = 0
+#     while True:
+#         try:
+#             print("Entro en while!")
+#             following = key_store.api.GetFriends(screen_name='coralpintado')
+#             print("Abro conexion!")
+#             requests += 1
+#             print('Requests: ', requests)
+#         except TwitterError as e:
+#             if str(e.message) in ("[{'message': 'Rate limit exceeded', 'code': 88}]",
+#                                   "[{'code': 88, 'message': 'Rate limit exceeded'}]"):
+#                 tries += 1
+#                 if tries == len(key_store.keys):
+#                     print('All credentials limit reached, sleeping ...')
+#                     tries = 0
+#                     time.sleep(30)
+#                 else:
+#                     requests = 0
+#                     print('Rate limit reached with credential ', key_store.idx, ' trying next ...')
+#                     key_store.change_credentials()
+#         except:
+#             print('Generic error, retrying in one minute')
+#             time.sleep(60)
+
 
 
 
 if __name__ == '__main__':
-    #Logging
-    api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret,
-                       access_token_key=access_token, access_token_secret=access_token_secret)
-
-    users = api.GetFriends(screen_name='coralpintado') # , cursor=cursor
+    key_store = KeyStore('../../twitter_keys.txt')
+    tries = 0
+    requests = 0
 
 
-    # ALMACENAMIENTO DE DATOS DE USUARIO: Lo ideal sería recorrer users e ir creando nodos con atributos para cada uno
+    # following = key_store.api.GetFriendIDs(screen_name='coralpintado')
+    following = key_store.api.GetFriendIDs(screen_name='manubarba')
+
+    # followers = key_store.api.GetFollowers(screen_name='coralpintado')
+    followers = key_store.api.GetFollowers(screen_name='manubarba')
+
+    print("following y followers almacenados")
+
+    i = 1
+
+    dict_json = OrderedDict()
+    lst_nodes = []
+
+    lst_dict_json = []
+    # dict_json['id'] = []
+    # dict_json['name'] = []
+    # dict_json['n_followers'] = []
+    # dict_json['n_friends'] = []
+    # dict_json['img'] = []
+    # dict_json['alias'] = []
+    # dict_json['n_tweets'] = []
+    # dict_json['geo'] = []
+    # dict_json['location'] = []
+    # dict_json['verified'] = []
+
+    # with open('data_twitter_C.json', 'w') as f:
+    #     print("fichero abierto")
+    with open('data_twitter_M.json', 'w') as f:
+        for u in followers:
+            for u2 in following:
+                if (u.id == u2) and (not u.protected):
+                    try:
+                        print("Usuario de following: ", u2)
+                        print("Entro en try")
+                        # Número de usuario -> Crear
+                        # dict_json['id'] = i
+                        dict_json['id'] = u.id
+                        # dict_json['id'].append(i)
+                        i += 1
+                        # Nombre
+                        dict_json['name'] = u.name
+                        # dict_json['name'].append(u.name)
+                        # Número de seguidores
+                        dict_json['n_followers'] = u.followers_count
+                        # dict_json['n_followers'].append(u.followers_count)
+                        # Número de personas a las que sigue el usuario
+                        dict_json['n_friends'] = u.friends_count
+                        # dict_json['n_friends'].append(u.friends_count)
+                        # Enlace http a la imagen del usuario
+                        dict_json['img'] = u.profile_image_url
+                        # dict_json['img'].append(u.profile_image_url)
+                        # Alias (nombre tras @)
+                        dict_json['alias'] = u.screen_name
+                        # dict_json['alias'].append(u.screen_name)
+                        # Número de tweets del usuario
+                        dict_json['n_tweets'] = u.statuses_count
+                        # dict_json['n_tweets'].append(u.statuses_count)
+                        # Geolocalización de tweets
+                        dict_json['geo'] = u.geo_enabled
+                        # dict_json['geo'].append(u.geo_enabled)
+                        # User location -> Useful?
+                        dict_json['location'] = u.location
+                        # dict_json['location'].append(u.location)
+                        # Cuenta verificada
+                        dict_json['verified'] = u.verified
+                        # dict_json['verified'].append(u.verified)
+
+                        print("Guardo following de user")
+                        # Almacenamiento de seguidos por el usuario
+                        # dict_json['following'] = key_store.api.GetFriends(screen_name=u.screen_name)
+                        dict_json['following'] = key_store.api.GetFriendIDs(screen_name=u.screen_name)
+
+                        print("following de user guardado")
+
+                        lst_dict_json.append(dict_json.copy())
+
+                        # Se añade el usuario a la lista de nodos
+                        lst_nodes.append(u.screen_name)
+
+                    except TwitterError as e:
+                        if str(e.message) in ("[{'message': 'Rate limit exceeded', 'code': 88}]",
+                                              "[{'code': 88, 'message': 'Rate limit exceeded'}]"):
+                            tries += 1
+                            if tries == len(key_store.keys):
+                                print('All credentials limit reached, sleeping ...')
+                                tries = 0
+                                time.sleep(30)
+                            else:
+                                requests = 0
+                                print('Rate limit reached with credential ', key_store.idx, ' trying next ...')
+                                key_store.change_credentials()
+                    except:
+                        print('Generic error, retrying in one minute')
+                        time.sleep(60)
 
 
 
 
-    # data = pd.DataFrame()
+        # Volcado a fichero json
+        # f.write(json.dumps(lst_dict_json))
 
-    for u in users:
+        for user in lst_dict_json:
+            f.write(json.dumps(user, ensure_ascii=False) + '\n')
 
-        # Nombre
-        u.name
-        # Número de seguidores
-        # u.followers_count
-        # Número de personas a las que sigue el usuario
-        u.friends_count
-        # Fecha de creación de la cuenta
-        u.created_at
-        # Número de favoritos que ha marcado el usuario
-        u.favourites_count
-        # Enlace http a la imagen del usuario
-        # u.profile_image_url
-        # Si True -> El usuario tiene los tweets protegidos
-        # u.protected
-        # Alias (nombre tras @)
-        u.screen_name
-        # Número de tweets del usuario
-        u.statuses_count
+    # for user in lst_nodes:
+    #     following_user = api.GetFriends(screen_name=user)
+    #     followers_user = api.GetFollowers(screen_name=user)
 
+    # print(lst_nodes)
+
+    # print(json.dumps(lst_dict_json))
+    # for item in lst_nodes:
+    #     print(item)
+
+    # for item in lst_dict_json:
+    #     print(item)
+    # print(lst_dict_json)
